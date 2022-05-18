edf1f7b85035edf1ef587e56839c49d9c263772f
Kevin Dai <kevin923.dai@gmail.com>
2022-05-18 17:54:25 -0500

    finished heapsort

--- src/SortingAlgorithms/heapSort.js
+++ src/SortingAlgorithms/heapSort.js
@@ -1,7 +1,6 @@
 export function getHeapSortAnimations(array){
     const animations = [];
 
-    window.alert(array);
 
     if (array.length <= 1) return array;
     
@@ -14,20 +13,28 @@ function heapify(array, n, i, animations){
     let l = 2 * i + 1;
     let r = 2 * i + 2;
 
+    animations.push([i,"yellow",l,"yellow",r,"yellow"]);
+
     if(l < n && array[l] > array[max]){
         max = l;
+        animations.push([i,"pink",l,"pink",r,"#ADD8E6"]);
     }
     if(r < n && array[r] > array[max]){
         max = r;
+        animations.push([i,"pink",l,"#ADD8E6",r,"pink"]);
     }
     
     if(max!==i){
+        animations.push([i,"pink",array[max],max,"pink",array[i],true]);
+        animations.push([i,"#ADD8E6",array[max],max,"#ADD8E6",array[i],true]);
         let temp = array[i];
         array[i]=array[max];
         array[max] = temp;
+        
 
         heapify(array,n,max,animations);
     }
+    animations.push([i,"#ADD8E6",l,"#ADD8E6",r,"#ADD8E6"]);
 
 }
 
@@ -39,6 +46,8 @@ function doHeap(array, animations){
     }
 
     for(let i = n-1; i>=0; i--){
+        animations.push([0,"pink",i,"pink"]);
+        animations.push([0,"#ADD8E6",array[i],i,"#ADD8E6",array[0],true]);
         let temp = array[0];
         array[0]=array[i];
         array[i]=temp;
